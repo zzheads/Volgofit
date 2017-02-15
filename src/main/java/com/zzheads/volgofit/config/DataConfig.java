b814afc09d0fa3c2d3a225569bdf48297e7784d5
Alexey Papin <zzheads@gmail.com>
2017-02-15 12:29:00 +0300

    Added UTF-8 support. Made CustomDataSource

--- src/main/java/com/zzheads/volgofit/config/DataConfig.java
+++ src/main/java/com/zzheads/volgofit/config/DataConfig.java
@@ -4,6 +4,7 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 @Configuration
@@ -19,16 +20,16 @@ public class DataConfig {
     @Bean
     public CustomDataSource dataSource() throws URISyntaxException {
 //        Local dataSource creds
-        String dbUrl = "jdbc:mysql://localhost/volgofit?useUnicode=yes&characterEncoding=UTF-8";
-        String username = "root";
-        String password = "xela1723014220";
+//        String dbUrl = "jdbc:mysql://localhost/volgofit?useUnicode=yes&characterEncoding=UTF-8";
+//        String username = "root";
+//        String password = "xela1723014220";
 
 //        Heroku deploing credentials
-//        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath() + "?useUnicode=yes&characterEncoding=UTF-8";
+        URI dbUri = new URI(System.getenv("CLEARDB_DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath() + "?useUnicode=yes&characterEncoding=UTF-8";
 
         CustomDataSource customDataSource = new CustomDataSource();
         customDataSource.setUrl(dbUrl);
