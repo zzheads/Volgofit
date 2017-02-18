87738d151399af08009815c0de14eca804148b6f
Alexey Papin <zzheads@gmail.com>
2017-02-18 10:55:20 +0300

    Added ImagesDirectory class

--- src/main/java/com/zzheads/volgofit/Application.java
+++ src/main/java/com/zzheads/volgofit/Application.java
@@ -1,11 +1,8 @@
 package com.zzheads.volgofit;
 
+import com.zzheads.volgofit.model.Imageable.ImagesDirectory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.File;
-
-import static com.zzheads.volgofit.model.Imageable.Imageable.DIR_NAME;
 
 //
 //  created by zzheads on 09.02.17
@@ -14,15 +11,7 @@ import static com.zzheads.volgofit.model.Imageable.Imageable.DIR_NAME;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        File imagesDir = new File(DIR_NAME);
-        if (!imagesDir.exists()) {
-            if (imagesDir.mkdir())
-                System.out.printf("Directory %s created.", imagesDir.getAbsolutePath());
-            else
-                System.out.printf("Can not create directory %s.", imagesDir.getAbsolutePath());
-        }
+        new ImagesDirectory().createIfNeeded(true);
         SpringApplication.run(Application.class, args);
     }
-
-
 }
